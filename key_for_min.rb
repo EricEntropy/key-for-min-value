a7ec42c5d60274be9a0c6be789a36cca51f6b564
Eric <contreras.eric18@gmail.com>
2021-01-19 22:28:22 -0500

    commit tree 715422fd2514a942b157612a8e225e9e26180433

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    values = []
+    #put the values from hash into array
+    name_hash.collect do |key, value|
+        values << value
+    end   
+    #find min value in values array, set it to equal the very first one
+    min = values[0]
+    values.each do |x| 
+        if x < min 
+            min = x
+        end
+    end
+    #function to return specific key given a specific value 
+    return name_hash.key(min)
 end
